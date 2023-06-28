8c0344c79487436421977411dca795e98fd882e2
abianfikri <abronjoy46@gmail.com>
2023-06-29 01:38:15 +0700

    model dokumentasi

---
+++ lib/model/dokumentasi_model.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+class DokumentasiModel {
+  String? id;
+  final String namaAsisten;
+  final String namaMatkul;
+  final String tanggal;
+  final String jam;
+  final String uid;
+  DokumentasiModel({
+    this.id,
+    required this.namaAsisten,
+    required this.namaMatkul,
+    required this.tanggal,
+    required this.jam,
+    required this.uid,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'namaAsisten': namaAsisten,
+      'namaMatkul': namaMatkul,
+      'tanggal': tanggal,
+      'jam': jam,
+      'uid': uid,
+    };
+  }
+
+  factory DokumentasiModel.fromMap(Map<String, dynamic> map) {
+    return DokumentasiModel(
+      id: map['id'],
+      namaAsisten: map['namaAsisten'] ?? '',
+      namaMatkul: map['namaMatkul'] ?? '',
+      tanggal: map['tanggal'] ?? '',
+      jam: map['jam'] ?? '',
+      uid: map['uid'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory DokumentasiModel.fromJson(String source) =>
+      DokumentasiModel.fromMap(json.decode(source));
+}
