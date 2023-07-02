86ca56f9370500a5f70f1a500547bd41253e2566
abianfikri <abronjoy46@gmail.com>
2023-07-02 13:17:54 +0700

    remove nameAsisten and Add imageUrl

--- lib/model/dokumentasi_model.dart
+++ lib/model/dokumentasi_model.dart
@@ -2,39 +2,39 @@ import 'dart:convert';
 
 class DokumentasiModel {
   String? id;
-  final String namaAsisten;
   final String namaMatkul;
   final String tanggal;
   final String jam;
   String? uid;
+  String? image;
   DokumentasiModel({
     this.id,
-    required this.namaAsisten,
     required this.namaMatkul,
     required this.tanggal,
     required this.jam,
     this.uid,
+    this.image,
   });
 
   Map<String, dynamic> toMap() {
     return {
       'id': id,
-      'namaAsisten': namaAsisten,
       'namaMatkul': namaMatkul,
       'tanggal': tanggal,
       'jam': jam,
       'uid': uid,
+      'image': image,
     };
   }
 
   factory DokumentasiModel.fromMap(Map<String, dynamic> map) {
     return DokumentasiModel(
       id: map['id'],
-      namaAsisten: map['namaAsisten'] ?? '',
       namaMatkul: map['namaMatkul'] ?? '',
       tanggal: map['tanggal'] ?? '',
       jam: map['jam'] ?? '',
       uid: map['uid'],
+      image: map['image'],
     );
   }
 
