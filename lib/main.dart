5b6047171cf85e2011b4003c935fb5f5ab14862d
abianfikri <abronjoy46@gmail.com>
2023-07-04 00:41:44 +0700

    change ui Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:final_exam_project/component/gradiant_text.dart';
 import 'package:final_exam_project/main_page.dart';
 import 'package:final_exam_project/view/halaman_login.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -38,35 +39,78 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
-
-    Future.delayed(Duration(seconds: 3)).then((value) {
-      Navigator.of(context).pushReplacement(
-        CupertinoPageRoute(
-          builder: (context) => MainPage(),
-        ),
-      );
-    });
+    Future.delayed(Duration(seconds: 3)).then(
+      (value) {
+        Navigator.of(context).pushReplacement(
+          CupertinoPageRoute(
+            builder: (context) => MainPage(),
+          ),
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        width: double.infinity,
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.purple,
+              Colors.red,
+              Colors.yellow.shade600,
+            ],
+          ),
+        ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: const [
-            Image(
-              image: AssetImage('assets/image/logo.jpeg'),
-              width: 300,
+            SizedBox(
+              height: 150,
+            ),
+            GradientText(
+              'Welcome to DocAsdos',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              gradient: LinearGradient(
+                colors: [
+                  Colors.tealAccent,
+                  Colors.white,
+                  Colors.tealAccent,
+                ],
+              ),
             ),
             SizedBox(
-              height: 50,
+              height: 80,
+            ),
+            CircleAvatar(
+              backgroundImage: AssetImage('assets/image/logo.jpeg'),
+              radius: 60,
+            ),
+            SizedBox(
+              height: 80,
             ),
             SpinKitSpinningLines(
-              color: Colors.orange,
+              color: Colors.white,
               size: 50.0,
-            )
+            ),
+            SizedBox(
+              height: 80,
+            ),
+            GradientText(
+              'Created by Abian',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              gradient: LinearGradient(
+                colors: [
+                  Colors.tealAccent,
+                  Colors.white,
+                  Colors.tealAccent,
+                ],
+              ),
+            ),
           ],
         ),
       ),
