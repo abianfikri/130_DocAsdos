1672d1871a2916b2b48b9df90d2fd6c2f161166c
abianfikri <abronjoy46@gmail.com>
2023-07-04 00:44:30 +0700

    change ui AddMatkuliah and create validator

--- lib/view/admin/add_matakuliah.dart
+++ lib/view/admin/add_matakuliah.dart
@@ -1,4 +1,6 @@
+import 'package:final_exam_project/component/gradiant_text.dart';
 import 'package:final_exam_project/controller/matakuliah_controller.dart';
+import 'package:final_exam_project/main_page.dart';
 import 'package:final_exam_project/model/matakuliah_model.dart';
 import 'package:final_exam_project/view/admin/matakuliah_page.dart';
 import 'package:final_exam_project/view/halaman_login.dart';
@@ -23,174 +25,222 @@ class _AddMatakuliahState extends State<AddMatakuliah> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue.shade800,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Form(
-            key: formkey,
-            child: Container(
-              height: 735,
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-                ),
+      appBar: AppBar(
+        title: Text('Add Matakuliah'),
+        centerTitle: true,
+        backgroundColor: Colors.pink.shade900,
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: formkey,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color(0xFF8A2387),
+                  Color(0xFFE94057),
+                  Color(0xFFF27121),
+                ],
               ),
-              padding: EdgeInsets.all(30),
-              child: Column(
-                children: [
-                  Row(
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  height: 530,
+                  width: 370,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
                     children: [
-                      FloatingActionButton(
-                        child: Icon(Icons.arrow_back),
-                        backgroundColor: Colors.deepOrange,
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => MatakuliahPage()));
-                        },
+                      SizedBox(
+                        height: 30,
                       ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(20),
-                    child: Text(
-                      'Tambah Matakuliah',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
+                      GradientText(
+                        'Matakuliah',
+                        style: TextStyle(
+                            fontSize: 25, fontWeight: FontWeight.bold),
+                        gradient: LinearGradient(
+                          colors: [
+                            Colors.blue,
+                            Colors.red,
+                            Colors.black,
+                          ],
+                        ),
                       ),
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(20),
-                    width: 350,
-                    child: TextFormField(
-                      keyboardType: TextInputType.name,
-                      decoration: InputDecoration(
-                        labelText: 'Nama Matkul',
-                        hintText: 'Enter name matkul',
+                      SizedBox(
+                        height: 20,
                       ),
-                      onChanged: (value) {
-                        namaMatkul = value;
-                      },
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(20),
-                    width: 350,
-                    child: TextFormField(
-                      keyboardType: TextInputType.name,
-                      decoration: InputDecoration(
-                        labelText: 'Nama Dosen',
-                        hintText: 'Enter name Dosen',
+                      // Nama Matkul
+                      Container(
+                        width: 280,
+                        child: TextFormField(
+                          keyboardType: TextInputType.name,
+                          decoration: InputDecoration(
+                            labelText: 'Nama Matkul',
+                            hintText: 'Enter name matkul',
+                          ),
+                          onChanged: (value) {
+                            namaMatkul = value;
+                          },
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Please enter Name Matkul";
+                            }
+                          },
+                        ),
                       ),
-                      onChanged: (value) {
-                        namaDosen = value;
-                      },
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(20),
-                    width: 350,
-                    child: TextFormField(
-                      keyboardType: TextInputType.name,
-                      decoration: InputDecoration(
-                        labelText: 'Semester',
-                        hintText: 'Enter semester',
+                      SizedBox(
+                        height: 20,
                       ),
-                      onChanged: (value) {
-                        semester = value;
-                      },
-                    ),
-                  ),
-                  SizedBox(
-                    height: 50,
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(20),
-                    child: InkWell(
-                      onTap: () async {
-                        // Save matakuliah
-                        if (formkey.currentState!.validate()) {
-                          MatakuliahModel mk = MatakuliahModel(
-                              namaMatkul: namaMatkul!,
-                              namaDosen: namaDosen!,
-                              semester: semester!);
-                          mkctr.addContact(mk);
+                      // Nama Dosen
+                      Container(
+                        width: 280,
+                        child: TextFormField(
+                          keyboardType: TextInputType.name,
+                          decoration: InputDecoration(
+                            labelText: 'Nama Dosen',
+                            hintText: 'Enter name Dosen',
+                          ),
+                          onChanged: (value) {
+                            namaDosen = value;
+                          },
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Please enter Name Dosen';
+                            }
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      // Semester
+                      Container(
+                        width: 280,
+                        child: TextFormField(
+                          keyboardType: TextInputType.name,
+                          decoration: InputDecoration(
+                            labelText: 'Semester',
+                            hintText: 'Enter semester',
+                          ),
+                          onChanged: (value) {
+                            semester = value;
+                          },
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Please enter Semester';
+                            }
+                          },
+                        ),
+                      ),
 
-                          //successful
-                          showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return AlertDialog(
-                                title: const Text('Berhasil Menambahkan data'),
-                                actions: <Widget>[
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  MatakuliahPage()));
-                                      // Navigate to the next screen or perform any desired action
-                                    },
-                                    child: const Text('OK'),
-                                  ),
-                                ],
-                              );
-                            },
-                          );
-                        } else {
-                          //  Failed
-                          showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return AlertDialog(
-                                title: const Text('Gagal menambahkan Data'),
-                                actions: <Widget>[
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  AddMatakuliah()));
-                                      // Navigate to the next screen or perform any desired action
-                                    },
-                                    child: const Text('OK'),
-                                  ),
-                                ],
-                              );
-                            },
-                          );
-                        }
-                      },
-                      child: Container(
-                        width: 300,
+                      SizedBox(
                         height: 50,
-                        decoration: BoxDecoration(
-                            color: Colors.deepOrange,
-                            borderRadius: BorderRadius.circular(50)),
-                        child: Center(
-                          child: Text(
-                            'Submit',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 17,
-                                fontWeight: FontWeight.bold),
+                      ),
+
+                      // Button Add
+                      Container(
+                        child: InkWell(
+                          onTap: () async {
+                            // Save matakuliah
+                            if (formkey.currentState!.validate()) {
+                              MatakuliahModel mk = MatakuliahModel(
+                                  namaMatkul: namaMatkul!,
+                                  namaDosen: namaDosen!,
+                                  semester: semester!);
+                              var aa = mkctr.addMatkul(mk);
+
+                              if (aa != null) {
+                                Navigator.pop(context, true);
+                              }
+
+                              //successful
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title:
+                                        const Text('Berhasil Menambahkan data'),
+                                    actions: <Widget>[
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.pop(context, true);
+                                          // Navigate to the next screen or perform any desired action
+                                        },
+                                        child: const Text('OK'),
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                            } else {
+                              //  Failed
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: const Text('Gagal menambahkan Data'),
+                                    actions: <Widget>[
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                          // Navigate to the next screen or perform any desired action
+                                        },
+                                        child: const Text('OK'),
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                            }
+                          },
+                          child: Container(
+                            width: 250,
+                            height: 50,
+                            decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomRight,
+                                    colors: [
+                                      Colors.purple,
+                                      Colors.red,
+                                      Colors.orange
+                                    ]),
+                                borderRadius: BorderRadius.circular(50)),
+                            child: Center(
+                              child: GradientText(
+                                'Submit',
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                gradient: LinearGradient(
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomRight,
+                                    colors: [
+                                      Colors.white,
+                                      Colors.white,
+                                      Colors.white
+                                    ]),
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
