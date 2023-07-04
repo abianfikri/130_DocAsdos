cbfc25d54eea72377969824b6193aa1f97ab0c4e
abianfikri <abronjoy46@gmail.com>
2023-07-04 13:41:35 +0700

    change ui homeAsisten become profile

--- lib/view/asisten/home_asisten.dart
+++ lib/view/asisten/home_asisten.dart
@@ -1,14 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:final_exam_project/component/gradiant_text.dart';
 import 'package:final_exam_project/controller/auth_controller.dart';
-import 'package:final_exam_project/controller/dokumentasi_controller.dart';
-import 'package:final_exam_project/controller/matakuliah_controller.dart';
-import 'package:final_exam_project/view/asisten/add_dokumentasi.dart';
-import 'package:final_exam_project/view/asisten/update_dokumentasi.dart';
 import 'package:final_exam_project/view/halaman_login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class HomeAsisten extends StatefulWidget {
   const HomeAsisten({super.key});
@@ -18,7 +13,6 @@ class HomeAsisten extends StatefulWidget {
 }
 
 class _HomeAsistenState extends State<HomeAsisten> {
-  var dkctr = DokumentasiController();
   final autctr = AuthController();
 
   final user = FirebaseAuth.instance.currentUser!;
@@ -27,246 +21,177 @@ class _HomeAsistenState extends State<HomeAsisten> {
       FirebaseFirestore.instance.collection('users');
 
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    dkctr.getDokumentasi();
-    autctr.getCurrentUser();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue.shade900,
+      backgroundColor: Colors.pink.shade700,
       body: SafeArea(
-        child: Column(
-          children: [
-            // Tulisan Welcome Asisten
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              height: 150,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      CircleAvatar(
-                        backgroundColor: Colors.black,
-                        radius: 30,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            // Username
-                            FutureBuilder<DocumentSnapshot>(
-                              future: userCollection.doc(user.uid).get(),
-                              builder: (context, snapshot) {
-                                if (!snapshot.hasData) {
-                                  return CircularProgressIndicator();
-                                }
-
-                                final username = snapshot.data?['username'];
-
-                                return Text(
-                                  'Hello, ' + username.toString().toUpperCase(),
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                );
-                              },
-                            ),
-                            // Asisten Text
-                            Text(
-                              "Asisten",
-                              style: TextStyle(
-                                fontSize: 16,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                  // Column Button Logout
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        child: IconButton(
-                          icon: Icon(Icons.logout),
-                          onPressed: () {
-                            // Logout
-                            autctr.signOut();
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => HalamanLogin(),
-                              ),
-                            );
-                          },
-                        ),
-                      )
-                    ],
-                  ),
-                ],
-              ),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color(0xFF8A2387),
+                Color(0xFFE94057),
+                Color(0xFFF27121),
+              ],
             ),
-            // List Data Dokumentasi Asissten
-            SizedBox(
-              child: Container(
-                margin: EdgeInsets.all(20),
-                child: Text(
-                  "Dokumentasi List",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-            // List Data Dokumentasi
-            Expanded(
-              child: Container(
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Tulisan Welcome Asisten
+              Container(
+                height: 400,
+                width: 380,
                 decoration: BoxDecoration(
                   color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
-                  ),
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                child: StreamBuilder<List<DocumentSnapshot>>(
-                  stream: dkctr.stream,
-                  builder: (context, snapshot) {
-                    if (!snapshot.hasData) {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 20,
+                    ),
+                    GradientText(
+                      "Selamat Datang",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      gradient: LinearGradient(
+                        colors: [
+                          Colors.teal,
+                          Colors.purple,
+                          Colors.red,
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    // Container Tampilan Username dan Admin Serta Logout
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                      height: 170,
+                      // Row Username, admin button Logout
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          // Column Username
+                          Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Row(
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      FutureBuilder<DocumentSnapshot>(
+                                        future:
+                                            userCollection.doc(user.uid).get(),
+                                        builder: (context, snapshot) {
+                                          if (!snapshot.hasData) {
+                                            return CircularProgressIndicator();
+                                          }
 
-                    final List<DocumentSnapshot> data = snapshot.data!;
+                                          final username =
+                                              snapshot.data?['username'];
 
-                    return ListView.builder(
-                      itemCount: data.length,
-                      itemBuilder: (context, index) {
-                        if (data[index]['uid'] == user.uid) {
-                          return Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: InkWell(
-                              onLongPress: () {
-                                // Update Data
-
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => UpdateDokumentasi(
-                                      id: data[index]['id'].toString(),
-                                      beforeAsisten:
-                                          data[index]['namaAsisten'].toString(),
-                                      beforeJam: data[index]['jam'].toString(),
-                                      beforeTanggal:
-                                          data[index]['tanggal'].toString(),
-                                      beforeMatkul:
-                                          data[index]['namaMatkul'].toString(),
-                                      uid: data[index]['uid'].toString(),
-                                    ),
-                                  ),
-                                );
-                              },
-                              child: Card(
-                                elevation: 15,
-                                child: ListTile(
-                                  leading: Container(
-                                    child: data[index]['image'] != null
-                                        ? Image.network(
-                                            '${data[index]['image'].toString()}')
-                                        : Container(
-                                            child: CircleAvatar(
-                                            backgroundColor: Colors.black,
-                                          )),
-                                  ),
-                                  title: Text(data[index]['namaMatkul']),
-                                  subtitle: Text(data[index]['tanggal']),
-                                  trailing: IconButton(
-                                    icon: Icon(Icons.delete),
-                                    onPressed: () {
-                                      // Delete
-                                      var aa = dkctr.deleteDokumentasi(
-                                          data[index]['id'].toString());
-                                      if (dkctr.deleteDokumentasi(
-                                              data[index]['id'].toString()) !=
-                                          null) {
-                                        dkctr
-                                            .deleteDokumentasi(
-                                                data[index]['id'].toString())
-                                            .then(
-                                          (value) {
-                                            setState(() {
-                                              dkctr.getDokumentasi();
-                                            });
-                                          },
-                                        );
-                                        Future.delayed(Duration(seconds: 2))
-                                            .then(
-                                          (value) =>
-                                              ScaffoldMessenger.of(context)
-                                                  .showSnackBar(
-                                            const SnackBar(
-                                              content:
-                                                  Text('Dokumentasi Deleted'),
+                                          return Text(
+                                            'Hello, ' +
+                                                username
+                                                    .toString()
+                                                    .toUpperCase(),
+                                            style: TextStyle(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.black,
                                             ),
-                                          ),
-                                        );
-                                      } else {
-                                        //  Failed
-                                        Future.delayed(Duration(seconds: 2))
-                                            .then(
-                                          (value) =>
-                                              ScaffoldMessenger.of(context)
-                                                  .showSnackBar(
-                                            const SnackBar(
-                                              content: Text(
-                                                  'Dokumentasi Failed to Delete'),
-                                            ),
-                                          ),
-                                        );
-                                      }
-                                    },
-                                  ),
+                                          );
+                                        },
+                                      ),
+                                      Text(
+                                        user.email!,
+                                        style: TextStyle(
+                                            fontSize: 14, color: Colors.grey),
+                                      ),
+                                      Text(
+                                        "Asisten",
+                                        style: TextStyle(
+                                            fontSize: 16, color: Colors.grey),
+                                      )
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                          // Row Untuk buttton Logout
+                          Row(
+                            children: [
+                              // Container Logout
+                              Container(
+                                margin: EdgeInsets.all(5),
+                                child: CircleAvatar(
+                                  backgroundImage:
+                                      AssetImage('assets/image/logo.jpeg'),
+                                  radius: 30,
                                 ),
                               ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    // Button Logout
+                    GestureDetector(
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: 250,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          gradient: LinearGradient(
+                            begin: Alignment.centerLeft,
+                            end: Alignment.centerRight,
+                            colors: [
+                              Color(0xFF8A2387),
+                              Color(0xFFE94057),
+                              Color(0xFFF27121),
+                            ],
+                          ),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.all(12.0),
+                          child: Text(
+                            'Logout',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
                             ),
-                          );
-                        }
-                        return Container();
+                          ),
+                        ),
+                      ),
+                      onTap: () async {
+                        autctr.signOut();
+                        Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HalamanLogin()),
+                            (route) => false);
                       },
-                    );
-                  },
+                    ),
+                  ],
                 ),
               ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Button Add
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => AddDokumentasi(),
-            ),
-          );
-        },
-        heroTag: null,
-        child: Icon(
-          Icons.add,
+            ],
+          ),
         ),
       ),
     );
