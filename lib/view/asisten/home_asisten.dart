6eb6fba22062e27acd0a7388058ec5e469c5a562
abianfikri <abronjoy46@gmail.com>
2023-07-09 02:28:39 +0700

    get collection profile

--- lib/view/asisten/home_asisten.dart
+++ lib/view/asisten/home_asisten.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:final_exam_project/component/gradiant_text.dart';
 import 'package:final_exam_project/controller/auth_controller.dart';
+import 'package:final_exam_project/controller/profile_controller.dart';
+import 'package:final_exam_project/view/asisten/edit_profile_asisten.dart';
 import 'package:final_exam_project/view/halaman_login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -14,11 +16,22 @@ class HomeAsisten extends StatefulWidget {
 
 class _HomeAsistenState extends State<HomeAsisten> {
   final autctr = AuthController();
+  final pfctr = ProfileController();
 
   final user = FirebaseAuth.instance.currentUser!;
 
+  String? username;
+  String? matkul;
+
   final CollectionReference userCollection =
       FirebaseFirestore.instance.collection('users');
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    pfctr.getProfile();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +57,7 @@ class _HomeAsistenState extends State<HomeAsisten> {
             children: [
               // Tulisan Welcome Asisten
               Container(
-                height: 400,
+                height: 530,
                 width: 380,
                 decoration: BoxDecoration(
                   color: Colors.white,
@@ -72,9 +85,9 @@ class _HomeAsistenState extends State<HomeAsisten> {
                     ),
                     // Container Tampilan Username dan Admin Serta Logout
                     Container(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                      height: 170,
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      height: 150,
+
                       // Row Username, admin button Logout
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -97,8 +110,7 @@ class _HomeAsistenState extends State<HomeAsisten> {
                                             return CircularProgressIndicator();
                                           }
 
-                                          final username =
-                                              snapshot.data?['username'];
+                                          username = snapshot.data?['username'];
 
                                           return Text(
                                             'Hello, ' +
@@ -129,10 +141,10 @@ class _HomeAsistenState extends State<HomeAsisten> {
                               ),
                             ],
                           ),
-                          // Row Untuk buttton Logout
+                          // Row Logo
                           Row(
                             children: [
-                              // Container Logout
+                              // Container Logo
                               Container(
                                 margin: EdgeInsets.all(5),
                                 child: CircleAvatar(
@@ -146,9 +158,168 @@ class _HomeAsistenState extends State<HomeAsisten> {
                         ],
                       ),
                     ),
-                    SizedBox(
-                      height: 50,
+                    // Garis
+                    Container(
+                      width: 350,
+                      decoration: BoxDecoration(
+                          border: Border(
+                              bottom:
+                                  BorderSide(color: Colors.grey, width: 1.0))),
                     ),
+
+                    SizedBox(
+                      height: 40,
+                    ),
+
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: FutureBuilder<List<DocumentSnapshot>>(
+                        future: pfctr
+                            .getProfileList(), // Ganti dengan method yang mengembalikan future dari list profile
+                        builder: (context, snapshot) {
+                          if (!snapshot.hasData) {
+                            return CircularProgressIndicator();
+                          }
+
+                          final List<DocumentSnapshot> data = snapshot.data!;
+
+                          return ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: data.length,
+                            itemBuilder: (context, index) {
+                              final namaLengkap = data[index]['nama'];
+                              final nim = data[index]['nim'];
+                              final semester = data[index]['semester'];
+
+                              final List<dynamic> matkulList =
+                                  data[index]['matkul'];
+
+                              if (data[index]['uid'] == user.uid.toString() &&
+                                  matkulList != '') {
+                                return Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        // Nama Lengkap
+                                        namaLengkap == null || namaLengkap == ""
+                                            ? Text(
+                                                'Nama Lengkap',
+                                                style: TextStyle(
+                                                  fontSize: 18,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              )
+                                            : Text(
+                                                namaLengkap,
+                                                style: TextStyle(
+                                                  fontSize: 18,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                        // NIM
+                                        nim == null || nim == ""
+                                            ? Text(
+                                                'NIM',
+                                                style: TextStyle(
+                                                  color: Colors.grey,
+                                                ),
+                                              )
+                                            : Text(
+                                                nim,
+                                                style: TextStyle(
+                                                  color: Colors.grey,
+                                                ),
+                                              ),
+                                        // Semester
+                                        semester == null || semester == ""
+                                            ? Text(
+                                                'Semester',
+                                                style: TextStyle(
+                                                  color: Colors.grey,
+                                                ),
+                                              )
+                                            : Text(
+                                                'Semester ' + semester,
+                                                style: TextStyle(
+                                                  color: Colors.grey,
+                                                ),
+                                              ),
+                                        Text("Asisten Dosen"),
+                                        SizedBox(
+                                          height: 5,
+                                        ),
+                                        // MatkulList
+                                        for (int i = 0;
+                                            i < matkulList.length;
+                                            i++)
+                                          Text(
+                                            '${i + 1}. ' +
+                                                matkulList[i].toString(),
+                                          ),
+                                      ],
+                                    ),
+                                    GestureDetector(
+                                      onTap: () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                EditProfileAsisten(
+                                              uid: user.uid,
+                                              username: username.toString(),
+                                              beforeNama: namaLengkap,
+                                              beforeNIM: nim,
+                                              beforeSemester: semester,
+                                              beforeMatkul: matkulList,
+                                            ),
+                                          ),
+                                        ).then((value) {
+                                          if (value == true) {
+                                            setState(() {
+                                              pfctr.getProfile();
+                                            });
+                                          }
+                                        });
+                                      },
+                                      child: Container(
+                                        alignment: Alignment.center,
+                                        width: 100,
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                          color: Colors.blue,
+                                        ),
+                                        child: Padding(
+                                          padding: EdgeInsets.all(8.0),
+                                          child: Text(
+                                            'Edit Profile',
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                );
+                              }
+                              return Container();
+                            },
+                          );
+                        },
+                      ),
+                    ),
+
+                    SizedBox(
+                      height: 80,
+                    ),
+
                     // Button Logout
                     GestureDetector(
                       child: Container(
